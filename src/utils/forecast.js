704eccc08f89efe60b16da8a9781a1f1ebb5b00b
James Lee <js44lee@edu.uwaterloo.ca>
2020-02-22 15:47:37 -0500

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,8 +12,11 @@ const forecast = (latitude, longitude, callback) => {
             const temperature = body.currently.temperature
             const chance = body.currently.precipProbability   
     
+            console.log(body.daily.data[0])
             callback(undefined, 
-                body.daily.data[0].summary + ` It is currently ${temperature} degrees out. There is a ${chance}\% chance of rain.`)
+                body.daily.data[0].summary + ` It is currently ${temperature} degrees out. There is a ${chance}\% chance of rain.\n` + 
+                'The high today is ' + body.daily.data[0].temperatureHigh + 
+                ' with a low of ' + body.daily.data[0].temperatureLow + '.')
         }
     })
 }
